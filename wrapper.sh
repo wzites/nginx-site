ad621562f4f9c1a69db89bc1409288fb64b2d41f
Leanne L. Caughorn <lcaughorn@gradual.gq>
2021-01-15 11:44:34 +0100

    added a brackground image and center the row vertically

--- wrapper.sh
+++ wrapper.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 ip=$(ifconfig eth0 | grep inet | sed -n -e 's/inet addr:/inet /' -e 's/^ *inet \([^ ]*\).*/\1/p;')
-echo "Nginx is running on http://$ip:80"
+echo "Your website is running at http://$ip/"
 
 exec nginx -g "daemon off;"
